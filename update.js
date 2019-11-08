6a62d016489c040a4044a65ed05b47c1d5d77a50
Jeff Escalante <jescalan@users.noreply.github.com>
2019-11-07 19:13:07 -0500

    initial commit

---
+++ update.js
@@ -0,0 +1,45 @@
+const signale = require('signale')
+const fs = require('fs-extra')
+const path = require('path')
+const prettier = require('prettier')
+const { loadSchema } = require('graphql-toolkit')
+const { codegen } = require('@graphql-codegen/core')
+const { printSchema, parse } = require('graphql')
+const { plugin: typescriptPlugin } = require('@graphql-codegen/typescript')
+const datoApiConfig = require('@hashicorp/nextjs-scripts/dato/config')
+
+async function main() {
+  signale.await('Fetching graphql schema from DatoCMS...')
+  const schema = await loadSchema(datoApiConfig.url, {
+    headers: datoApiConfig.headers,
+    method: 'POST' // patch until this is resolved: https://github.com/ardatan/graphql-toolkit/pull/318
+  })
+  signale.await('Creating Typescipt file...')
+  const outputFile = 'index.d.ts'
+  const config = {
+    filename: outputFile,
+    schema: parse(printSchema(schema)),
+    plugins: [
+      {
+        typescript: {} // Here you can pass configuration to the plugin
+      }
+    ],
+    pluginMap: {
+      typescript: {
+        plugin: typescriptPlugin
+      }
+    }
+  }
+  // codegen runs prettier by default, but it doesn't pick up our prettier
+  // config correctly, so we need to run it again
+  const prettierOptions = {
+    parser: 'typescript',
+    semi: false,
+    singlequote: false
+  }
+  const output = prettier.format(await codegen(config), prettierOptions)
+  fs.writeFileSync(outputFile, output)
+  signale.success('done!')
+}
+
+main()
